e71ed359881ac1e21a921133205caa3044ceb6a4
Lars Knickrehm <mail@lars-sh.de>
2019-02-08 10:02:51 +0100

    Remove @ToString from exceptions without fields

--- src/main/lombok/de/larssh/json/dom/JsonDomNotSupportedException.java
+++ src/main/lombok/de/larssh/json/dom/JsonDomNotSupportedException.java
@@ -7,7 +7,6 @@ import lombok.ToString;
 /**
  * Thrown to indicate that JSON DOM does not support a particular DOM feature.
  */
-@ToString
 public class JsonDomNotSupportedException extends DOMException {
 	// @EqualsAndHashCode(callSuper = true, onParam_ = { @Nullable })
 	private static final long serialVersionUID = 6374630768171198592L;
