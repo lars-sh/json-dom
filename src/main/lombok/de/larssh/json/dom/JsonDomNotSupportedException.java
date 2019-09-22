86a1f823d21081fd7fe0024d4a4929dfe4b79fb9
Lars Knickrehm <mail@lars-sh.de>
2019-09-22 21:15:52 +0200

    Remove commented code

--- src/main/lombok/de/larssh/json/dom/JsonDomNotSupportedException.java
+++ src/main/lombok/de/larssh/json/dom/JsonDomNotSupportedException.java
@@ -6,7 +6,6 @@ import org.w3c.dom.DOMException;
  * Thrown to indicate that JSON DOM does not support a particular DOM feature.
  */
 public class JsonDomNotSupportedException extends DOMException {
-	// @EqualsAndHashCode(callSuper = true, onParam_ = { @Nullable })
 	private static final long serialVersionUID = 6374630768171198592L;
 
 	/**
