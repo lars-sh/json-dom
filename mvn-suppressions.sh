5be2760b46a23d03934bfd5cb8b54e2735ebc8d6
Lars Knickrehm <mail@lars-sh.de>
2021-11-02 00:00:15 +0100

    Suppress: The JaCoCo Maven Plugin references a moved Maven artifact.

---
+++ mvn-suppressions.sh
@@ -0,0 +1,8 @@
+# JaCoCo Maven Plugin: The Maven Plugin is referencing a moved Maven artifact.
+function suppressJaCoCoMavenPlugin() {
+	cat < /dev/stdin \
+	| grep --invert-match --perl-regexp "^\\[WARNING\\] The artifact xml-apis:xml-apis:jar:2\\.0\\.2 has been relocated to xml-apis:xml-apis:jar:1\\.0\\.b2$"
+}
+
+cat < /dev/stdin \
+| suppressJaCoCoMavenPlugin
